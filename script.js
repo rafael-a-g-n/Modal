09363b0bb466af5892aa69e7f8b327026f5428c2
Rwedoll <ragn.ryzen@gmail.com>
2024-01-03 18:54:10 +0000

    Saved html elements to variables to work with.

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
