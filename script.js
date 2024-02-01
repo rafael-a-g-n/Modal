1586c6e1a1f337987d04a8680a0af17f2aff088b
Rwedoll <ragn.ryzen@gmail.com>
2024-02-01 18:06:27 +0000

    Each button now shows it's personal model, added close modal on ESC press.

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 
-function openModal() {
+function openModal(modal, btnCloseModal) {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
+  btnCloseModal.addEventListener('click', function () {
+    closeModal(modal);
+  });
 }
 
-function closeModal() {
+function closeModal(modal) {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 }
 
 for (let i = 0; i < btnsShowModal.length; i++) {
-  btnsShowModal[i].addEventListener('click', openModal);
+  btnsShowModal[i].addEventListener('click', function () {
+    const modal = document.querySelector(btnsShowModal[i].dataset.modalTarget);
+    const btnCloseModal = modal.querySelector('.close-modal');
+    openModal(modal, btnCloseModal);
+  });
 }
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+overlay.addEventListener('click', function () {
+  const modals = document.querySelectorAll('.modal');
+  modals.forEach(modal => {
+    if (!modal.classList.contains('hidden')) {
+      closeModal(modal);
+    }
+  });
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach(modal => {
+      if (!modal.classList.contains('hidden')) {
+        closeModal(modal);
+      }
+    });
+  }
+});
